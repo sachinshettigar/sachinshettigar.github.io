9a2ca6028baa435c6dc256c4b187f5b35bd394f9
Sachin Shettigar <sachinsp17@gmail.com>
2024-10-27 20:40:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-document.onload = function(){
+settimeout(() => {
+   
+  const ul = document.getElementById("timeLeft")
   
-const ul = document.getElementById("timeLeft")
-
-const start = 1992;
-const end=2992;
-const years = []
-
-for(year = start; year<= end; year++) {
-  let className = "";
-  if(year < 2024){
-    className = "lived"
+  const start = 1992;
+  const end=2992;
+  const years = []
+  
+  for(year = start; year<= end; year++) {
+    let className = "";
+    if(year < 2024){
+      className = "lived"
+    }
+    else if(year === 2024) {
+      className = "living"
+    }
+    else 
+      className = "yetToLive"
+    const el = `<li title={year} class={className}></li>`
+     years.push(el);
   }
-  else if(year === 2024) {
-    className = "living"
-  }
-  else 
-    className = "yetToLive"
-  const el = `<li title={year} class={className}></li>`
-   years.push(el);
-}
-
-ul.innerhtml = years.join("");
+  
+  ul.innerHTML = years.join("");
 }
