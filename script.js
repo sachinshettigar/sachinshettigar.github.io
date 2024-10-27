0aebb47d487b1739252d82da5705907b25fdaf4b
Sachin Shettigar <sachinsp17@gmail.com>
2024-10-27 20:45:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ settimeout(() => {
     }
     else 
       className = "yetToLive"
-    const el = `<li title={year} class={className}></li>`
+    const el = `<li title=${year} class=${className}></li>`
      years.push(el);
   }
   
